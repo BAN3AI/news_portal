d7e63ed6bd7434f598bdbf57c844300dda1106eb
BAN3AI <151779384+BAN3AI@users.noreply.github.com>
2024-08-11 17:41:16 +0300

    Update views.py

--- News/news/views.py
+++ News/news/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-# from django.views.generic.edit import FormView
 from  django.views.generic.base import View
 from .models import *
 
@@ -21,19 +20,13 @@ class PostDetail(View):
 
 
 def news_page_list(request):
-    """ Представление для вывода страницы с новостями по заданию D3.6 """
-
+    
     newslist = Post.objects.all().order_by('-rating')[:6]
 
     return render(request, 'news/news.html', {'newslist': newslist})
 
 
-# class Myform(FormView):
-#     form_class = myform
-#     success_url = "/succsess/"
-#
-#     def form_valid(self, form):
-#         return super().form_valid(form)
+
 
 
 def pageNotFound(request, exception):
